1c3f041a462ea7f6cc69d97775e51368908b70a4
StylishBoi <max@rdh.ch>
2026-05-30 11:50:52 +0200

    - Updated room layouts

--- Assets/01_Scripts/System/FloorRepeater.cs
+++ Assets/01_Scripts/System/FloorRepeater.cs
@@ -7,7 +7,8 @@ public class FloorRepeater : MonoBehaviour
     [SerializeField] private float speed = 1.0f;
     private GameObject[] _floorsPrefab;
     [SerializeField] private GameObject[] floor;
-    [Range (0, 2)] private int _floorIndex; 
+    [Range (0, 2)] private int _floorIndex;
+    private Quaternion _rotation = new Quaternion(0f, 180f, 0f, 0f);
 
     void Update()
     {
@@ -22,8 +23,16 @@ public class FloorRepeater : MonoBehaviour
             if (Vector3.Distance(onetilefloor.transform.position, targetPoint.transform.position) < 0.025f)
             {
                 Debug.Log("It has reached its destination");
-                Instantiate(floor[_floorIndex], spawnPoint.transform.position, floor[_floorIndex].transform.rotation, transform);
-
+                int randomIndex = Random.Range(0, 1);
+                switch (randomIndex)
+                {
+                    case 0:
+                        Instantiate(floor[_floorIndex], spawnPoint.transform.position, floor[_floorIndex].transform.rotation, transform);
+                        break;
+                    case 1:
+                        Instantiate(floor[_floorIndex], spawnPoint.transform.position, floor[_floorIndex].transform.rotation, transform);
+                        break;
+                }
                 _floorIndex = (_floorIndex + 1) % 3;
                 
                 Destroy(onetilefloor);
